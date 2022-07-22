a2be73246f255fcb43458388f5c4355e25b9a379
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-07-23 00:12:57 +0100

    Fixed authentication on give point Added intentional draw

--- router.go
+++ router.go
@@ -22,3 +22,13 @@ func Router() http.Handler {
 
 	return r
 }
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Authorization") != AUTH_CODE {
+			WriteErr(w, http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
